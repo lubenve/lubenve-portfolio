71e2280cacc5ed26ec377ee078bedc6376d8968b
Luna <www.lunabenitez@gmail.com>
2023-01-11 01:13:48 -0300

    agrego progreso

---
+++ public/js/javascript.js
@@ -0,0 +1,11 @@
+let oscuro = document.getElementById("moon");
+
+oscuro.addEventListener('click', ()=>{
+
+})
+/* let bg = document.getElementById("main_bg");
+
+window.addEventListener('scroll', () =>{
+    let scrolleo = window.scrollY;
+    bg.style.top = scrolleo + 0.25 + 'px';
+}); */
