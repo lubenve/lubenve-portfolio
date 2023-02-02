6388f363096ee76a2b7312d2d4f6b3322390b23c
Luna <www.lunabenitez@gmail.com>
2023-02-02 01:36:16 -0300

    mq

--- public/js/javascript.js
+++ public/js/javascript.js
@@ -1,11 +1,21 @@
-let oscuro = document.getElementById("moon");
+let menu = document.querySelector(".hamburguesa");
+let nav_izq = document.querySelector(".nav_izq");
 
-oscuro.addEventListener('click', ()=>{
-
+menu.addEventListener("click", ()=> {
+/*     menu.classList.toggle("active"); */
+    nav_izq.classList.toggle("active");
 })
+
+
+/* let oscuro = document.getElementById("moon");
+
+oscuro.addEventListener('click', ()=>{ */
+
+//})
 /* let bg = document.getElementById("main_bg");
 
 window.addEventListener('scroll', () =>{
     let scrolleo = window.scrollY;
     bg.style.top = scrolleo + 0.25 + 'px';
 }); */
+
